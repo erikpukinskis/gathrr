d9f73591f957fca7e749e676f929cb7266af2ec0
Erik Pukinskis <erik@x60erik.(none)>
2009-09-27 20:44:55 -0700

    initialized rspec, started getting Site.new to accept a list of feeds

--- app/models/site.rb
+++ app/models/site.rb
@@ -1,3 +1,23 @@
 class Site < ActiveRecord::Base
   has_many :feeds
+
+  def feed_list
+
+  end
+
+  def feed_list=(feed_string)
+    if self.new_record?
+      @feed_string = feed_string
+    else
+      self.create_feeds(feed_string)  # parse tags, create tags & taggings
+    end
+  end
+
+  def create_feeds(feed_string)
+      
+  end
+
+  def after_create
+    self.feed_list = @feed_string if @feed_string
+  end
 end
