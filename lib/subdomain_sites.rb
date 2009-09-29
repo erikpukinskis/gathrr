bfc3d21660674181a03449ef8223ed0d6a30ed39
Erik Pukinskis <erik@x60erik.(none)>
2009-09-28 18:04:41 -0700

    added some debugging printouts

--- lib/subdomain_sites.rb
+++ lib/subdomain_sites.rb
@@ -15,21 +15,25 @@ module SubdomainSites
     end
     
     def site_url( site_subdomain = default_site_subdomain, use_ssl = request.ssl? )
+      print "in site_url. site_subdomain = #{site_subdomain}\n"
       http_protocol(use_ssl) + site_host(site_subdomain)
     end
     
     def site_host( subdomain )
+      print "in site_host. subdomain = #{subdomain}\n"
       site_host = ''
       site_host << subdomain + '.'
       site_host << site_domain
     end
  
     def site_domain
+      print "in site_domain. request = #{request}\n"
       site_domain = ''
       site_domain << request.domain + request.port_string
     end
  
     def site_subdomain
+      print "in site_subdomain. subdomains = #{request.subdomains}\n"
       request.subdomains.first || ''
     end
     
@@ -38,6 +42,7 @@ module SubdomainSites
     end      
           
     def current_site
+      print "in current_site. site_subdomain = #{site_subdomain}\n"
       Site.find_by_slug(site_subdomain)
     end
     
